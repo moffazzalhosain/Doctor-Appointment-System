84af03c61f6a41080d21ef3f967d765668f268f1
fayezur7 <fayezur.rahman.cse@ulab.edu.bd>
2021-04-24 19:51:51 +0600

    Login Layout done

---
+++ app/src/main/java/com/example/doctorappointment/PatLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.doctorappointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pat_login);
+    }
+}
